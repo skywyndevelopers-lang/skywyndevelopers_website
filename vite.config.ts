82f530b740405ade9a0dba8eda5218b5ec50a289
skywyndevelopers <skywyndevelopers@gmail.com>
2025-10-25 18:24:00 +0530

    fix domain base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/skywyn_developers/", // 👈 must match your GitHub repo name
+  base: "/", // 👈 must match your GitHub repo name
   server: {
     host: "::",
     port: 8080,
